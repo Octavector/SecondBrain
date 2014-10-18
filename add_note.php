b41918102ce1f4aa98672d2b812004dc0365c68f
Octavector <hello@octavector.co.uk>
2014-10-18 14:14:16 +0100

    add sanitize function to ensure clean user input

--- add_note.php
+++ add_note.php
@@ -7,9 +7,11 @@ if(mysqli_ping($dbc)){
     echo 'MySQL Server'.mysqli_get_server_info($dbc).'on'.mysqli_get_host_info($dbc);
 }
 
+
 #Check if user entered required information
 if(!empty($_POST['addtitle'])){
     $noteTitle = $_POST['addtitle'];
+    $noteTitle = sanitize($noteTitle,$dbc);
 }
 else{  
     $noteTitle = "no info entered";
@@ -17,6 +19,7 @@ else{
 
 if(!empty($_POST['addnote'])){
     $noteBody = $_POST['addnote'];
+    $noteBody = sanitize($noteBody,$dbc);
 }
 else{
     $noteBody = 'no info entered!';
@@ -24,3 +27,26 @@ else{
 
 echo '<h1>'.$noteTitle.'</h1>';
 echo '<p>'.$noteBody.'</p>';
+
+
+#loop through tables
+$q = 'show tables';
+$r = mysqli_query($dbc,$q);
+
+if ($r){
+    while($row = mysqli_fetch_array($r,MYSQLI_NUM)){
+        echo '<br>'.$row[0];
+    }
+}
+mysqli_free_result($r);
+mysqli_close($dbc);
+
+
+#functions
+
+#Clean up user input
+function sanitize($toSanitize,$dbc){
+    $toSanitize = mysqli_real_escape_string($dbc,$toSanitize);
+    $toSanitize = strip_tags($toSanitize);
+    return $toSanitize;
+}
